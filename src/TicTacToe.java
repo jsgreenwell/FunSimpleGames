0e76f2ca130d003151501f122a40005286627127
emmettemmons <eemmons@millikin.edu>
2026-05-12 20:26:45 -0500

    Sets up the board and show a move being played

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicTacToe {
     // Boolean variable which is true until player wants to quit
     public boolean keepPlaying = true;
+
+    private char[][] board;
+    private List<String> moves;
+    private String player1;
+    private String player2;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        moves = new ArrayList<String>();
+        initialBoard();
+    }
+
+    public void initialBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    public void displayBoard() {
+        System.out.println();
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public boolean doingMove(int row, int col, char symbol) {
+
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+        if (board[row][col] != '-') {
+            return false;
+        }
+        board[row][col] = symbol;
+        moves.add(symbol + "Played" + row + "," + col);
+        return true;
+    }
+
 
     /**
      * Runs the main game loop - calling other functions as needed.
