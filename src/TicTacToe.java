bb5e7a295781df7e5df402331e0647ef0f86423b
emmettemmons <eemmons@millikin.edu>
2026-05-12 20:32:08 -0500

    Shows how a winner will be decided

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -49,6 +49,28 @@ public class TicTacToe {
         return true;
     }
 
+    public boolean winner(char symbol){
+        for (int row = 0; row < 3; row++) {
+            if(board[row][0] == symbol &&
+                board[row][1] == symbol &&
+                board[row][2] == symbol) {
+                return true;
+            }
+        }
+        for (int col = 0; col < 3; col++) {
+            if (board[0][col] == symbol &&
+                    board[1][col] == symbol &&
+                    board[2][col] == symbol) {
+                return true;
+            }
+            if (board[0][0] == symbol &&
+                board[1][1] == symbol &&
+                board[2][2] == symbol) {
+                return true;
+            }
+        }
+    }
+
 
     /**
      * Runs the main game loop - calling other functions as needed.
